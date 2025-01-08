7af2d4987b96b6d22400f83ccb49cd1d871616b9
akovtune <akovtune@student.codam.nl>
2025-01-08 14:26:10 +0100

    Created branch map_validator

---
+++ src/map_validator.c
@@ -0,0 +1,7 @@
+#include "map_validator.h"
+
+bool	is_valid_map(char **map)
+{
+	(void)map;
+	return (false);
+}
---
+++ src/map_validator.h
@@ -0,0 +1,18 @@
+#ifndef MAP_VALIDATOR_H
+# define MAP_VALIDATOR_H
+
+# include <stdbool.h>
+
+typedef struct map_data
+{
+	int		columns_count;
+	int		rows_count;
+	bool	is_a_rectangle;
+	bool	has_duplicates;
+	bool	has_invalid_chars;
+	bool	has_enough_components;
+	bool	is_surrouded_by_walls;
+	bool	has_a_valid_path;
+}			t_map_data;
+
+#endif
