9f7431e9dbaa17b28804300eaf2a8e35365d9c38
akovtune <akovtune@student.codam.nl>
2025-01-10 14:36:18 +0100

    Finished map_validator - v1 : - Now I'm able to validate maps on all types of errors but one: I can not check yet if a map doesn't have a valid path in it. - Created another branch for that purpose called path_checker

--- src/map_validator.c
+++ src/map_validator.c
@@ -1,7 +1,129 @@
 #include "map_validator.h"
 
-bool	is_valid_map(char **map)
+static bool	analyze_row(t_map_data *map_data, char *row);
+static bool	analyze_map(t_map_data *map_data);
+static bool	has_valid_cpe_amount(t_map_data *map_data);
+static bool	has_holes(t_map *map);
+
+/*
+Takes a map and returns:
+1 - if map is a valid map
+0 - if map is invalid
+-1 - on error
+*/
+int	is_valid_map(t_map *map)
 {
-	(void)map;
+	t_map_data	*map_data;
+	bool		result;
+
+	if (map->rows_count < 3)
+		return (0);
+	map->columns_count = ft_strlen(map->rows[0]);
+	if (map->columns_count < 5)
+		return (0);
+	map_data = init_map_data(map);
+	if (!map_data)
+		return (-1);
+	result = 1;
+	if (!analyze_map(map_data))
+		result = 0;
+	else if (!has_valid_cpe_amount(map_data))
+		result = 0;
+	else if (has_holes(map))
+		result = 0;
+	free(map_data);
+	return (result);
+}
+
+/*
+Checks whether a map has holes in the:
+- roof (first row)
+- base (last row)
+*/
+static bool	has_holes(t_map *map)
+{
+	if (index_of('0', map->rows[0]) != -1)
+		return (true);
+	else if (index_of('0', map->rows[map->rows_count - 1]) != -1)
+		return (true);
 	return (false);
+}
+
+/*
+Checks whether the map contains:
+- at least 1 collectible (C)
+- exactly 1 starting position (P)
+- exactly 1 exit (E)
+*/
+static bool	has_valid_cpe_amount(t_map_data *map_data)
+{
+	if (map_data->exits_count != 1)
+		return (false);
+	else if (map_data->players_count != 1)
+		return (false);
+	else if (map_data->map->collectibles_count < 1)
+		return (false);
+	return (true);
+}
+
+/*
+Goes through map row by row and:
+- checks whether it has invalid characters
+- checks whether it has holes in the walls from the sides
+- checks whether map is a rectangle
+- collects information about the map like:
+	- amount of players
+	- amount of collectables
+	- amount of exits
+
+Doesn't go to the next row if it finds any errors in the current one
+*/
+static bool	analyze_map(t_map_data *map_data)
+{
+	t_map	*map;
+	int		i;
+
+	map = map_data->map;
+	i = -1;
+	while (++i < map->rows_count)
+		if (!analyze_row(map_data, map->rows[i]))
+			return (false);
+	return (true);
+}
+
+/*
+Goes through the row and:
+- checks whether it has invalid characters
+- checks whether it has holes in the walls from the sides
+- checks whether map is a rectangle
+- collects information about the map like:
+	- amount of players
+	- amount of collectables
+	- amount of exits
+*/
+static bool	analyze_row(t_map_data *map_data, char *row)
+{
+	t_map	*map;
+	int		i;
+
+	map = map_data->map;
+	if (row[0] == '0')
+		map_data->is_surrouded_by_walls = false;
+	i = -1;
+	while (row[++i])
+	{
+		if (row[i] == 'C')
+			map->collectibles_count++;
+		else if (row[i] == 'P')
+			map_data->players_count++;
+		else if (row[i] == 'E')
+			map_data->exits_count++;
+		else if (row[i] == '0' && row[i + 1] == '\0')
+			map_data->is_surrouded_by_walls = false;
+		else if (!(row[i] == '1' || row[i] == '0'))
+			return (false);
+	}
+	if (i != map->columns_count)
+		map_data->is_a_rectangle = false;
+	return (map_data->is_a_rectangle && map_data->is_surrouded_by_walls);
 }
--- src/map_validator.h
+++ src/map_validator.h
@@ -1,21 +1,11 @@
 #ifndef MAP_VALIDATOR_H
 # define MAP_VALIDATOR_H
 
+# include "ft_string.h"
+# include "map_data.h"
 # include <stdbool.h>
 # include <stdlib.h>
 
-typedef struct map_data
-{
-	int		columns_count;
-	int		rows_count;
-	bool	is_a_rectangle;
-	bool	has_duplicates;
-	bool	has_invalid_chars;
-	bool	has_enough_components;
-	bool	is_surrouded_by_walls;
-	bool	has_a_valid_path;
-}			t_map_data;
-
-bool		is_valid_map(char **map);
+int	is_valid_map(t_map *map);
 
 #endif
