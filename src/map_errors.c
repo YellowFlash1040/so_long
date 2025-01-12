c2b888b6f17cc2002dafecd78a0d49795c3eeac3
akovtune <akovtune@student.codam.nl>
2025-01-12 14:57:07 +0100

    Created map_errors "library"

---
+++ src/map_errors.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_errors.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/11 18:30:09 by akovtune          #+#    #+#             */
+/*   Updated: 2025/01/12 13:49:29 by akovtune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "map_errors.h"
+
+t_map_errors	*init_map_errors(void)
+{
+	t_map_errors	*map_errors;
+
+	map_errors = (t_map_errors *)malloc(sizeof(t_map_errors));
+	if (!map_errors)
+		return (NULL);
+	map_errors->count = 0;
+	map_errors->has_holes = false;
+	map_errors->has_invalid_chars = false;
+	map_errors->has_minimum_dimensions = true;
+	map_errors->has_valid_cpe_amount = true;
+	map_errors->is_a_rectangle = true;
+	return (map_errors);
+}
+
+int	count_errors(t_map_errors *map_errors)
+{
+	int	count;
+
+	count = 0;
+	if (map_errors->has_holes)
+		count++;
+	if (map_errors->has_invalid_chars)
+		count++;
+	if (!map_errors->has_minimum_dimensions)
+		count++;
+	if (!map_errors->has_valid_cpe_amount)
+		count++;
+	if (!map_errors->is_a_rectangle)
+		count++;
+	return (count);
+}
+
+void	print_map_errors(t_map_errors *map_errors)
+{
+	ft_printf("\033[31mError\n");
+	if (map_errors->has_holes)
+		ft_printf("%s ❌\n", ERR_HOLES);
+	if (map_errors->has_invalid_chars)
+		ft_printf("%s ❌\n", ERR_INVALID_CHARS);
+	if (!map_errors->has_minimum_dimensions)
+		ft_printf("%s ❌\n", ERR_MAP_SIZE);
+	if (!map_errors->has_valid_cpe_amount)
+		ft_printf("%s ❌\n", ERR_CPE_AMOUNT);
+	if (!map_errors->is_a_rectangle)
+		ft_printf("%s ❌\n", ERR_MAP_SHAPE);
+	ft_printf("\033[0m");
+}
---
+++ src/map_errors.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_errors.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/12 12:21:43 by akovtune          #+#    #+#             */
+/*   Updated: 2025/01/12 13:49:21 by akovtune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAP_ERRORS_H
+# define MAP_ERRORS_H
+
+# include "ft_printf.h"
+# include <stdbool.h>
+# include <stdlib.h>
+
+typedef struct map_errors
+{
+	bool		has_minimum_dimensions;
+	bool		has_invalid_chars;
+	bool		has_holes;
+	bool		is_a_rectangle;
+	bool		has_valid_cpe_amount;
+	int			count;
+}				t_map_errors;
+
+t_map_errors	*init_map_errors(void);
+int				count_errors(t_map_errors *map_errors);
+void			print_map_errors(t_map_errors *map_errors);
+
+# define ERR_HOLES "The map must be closed/surrounded by walls"
+# define ERR_INVALID_CHARS "Valid map characters: 0, 1, C, E, P"
+# define ERR_MAP_SIZE "The map is too small"
+# define ERR_CPE_AMOUNT "The map must contain 1 E, at least 1 C, and 1 P"
+# define ERR_MAP_SHAPE "The map must be rectangular"
+
+#endif
