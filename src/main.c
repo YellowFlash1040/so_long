dd3b3569bd89f1c249b63c8f46fb40f48a29edac
akovtune <akovtune@student.codam.nl>
2025-01-16 17:37:59 +0100

    Updated the main.c in order to compile it all (graphics v1 and the map)

--- src/main.c
+++ src/main.c
@@ -6,20 +6,37 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 13:31:39 by akovtune          #+#    #+#             */
-/*   Updated: 2025/01/12 14:12:08 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/01/16 16:48:29 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "game.h"
 #include "map_toolkit.h"
 
-int	main(void)
+void	print_usage_message(void);
+
+int	main(int argsc, char **args)
 {
 	char	*filepath;
+	t_map	*map;
 	char	processing_result;
 
-	filepath = "maps/path_check/map5.ber";
-	processing_result = process_map(filepath);
+	if (argsc != 2)
+	{
+		print_usage_message();
+		return (1);
+	}
+	filepath = args[1];
+	// filepath = "maps/path_check/map5.ber";
+	processing_result = process_map_file(filepath, &map);
 	if (processing_result != 0)
 		return (processing_result);
+	start_game(map);
 	return (0);
+}
+
+void	print_usage_message(void)
+{
+	ft_printf("\033[31mError\n\033[0m");
+	ft_printf("\033[1;33mUsage: [binary_name] [map_file_path].ber\n\033[0m");
 }
