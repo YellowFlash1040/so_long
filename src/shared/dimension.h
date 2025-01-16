4d4f6e88d184050679a482df480a4ac7a40db323
akovtune <akovtune@student.codam.nl>
2025-01-16 17:36:47 +0100

    Created shared folder
    - Moved t_direction and t_dimension types from the path_checker.h
    to their own files inside the /shared folder. Because I needed
    them both: in /gui and in /map

---
+++ src/shared/dimension.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dimension.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 16:33:46 by akovtune          #+#    #+#             */
+/*   Updated: 2025/01/16 16:33:47 by akovtune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DIMENSION_H
+# define DIMENSION_H
+
+typedef struct dimensions
+{
+	int	width;
+	int	height;
+}		t_dimensions;
+
+#endif
