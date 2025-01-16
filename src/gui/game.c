24eed84ef357c26d5f755f6486362a229842a7dc
akovtune <akovtune@student.codam.nl>
2025-01-16 18:34:48 +0100

    Updated the game.c file. Changed the height of the window. I added previously a pdding of 30 px at the bottom in order to have place to display the movements count. But then I decided that I will no do it for now, and leave the bonus for later

--- src/gui/game.c
+++ src/gui/game.c
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 12:56:38 by akovtune          #+#    #+#             */
-/*   Updated: 2025/01/16 15:17:45 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/01/16 18:33:29 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	start_game(t_map *map)
 	if (!data)
 		return (1);
 	data->user = create_window(map->columns_count * SPRITE_WIDTH,
-			map->rows_count * SPRITE_HEIGHT + 30, TITLE);
+			map->rows_count * SPRITE_HEIGHT, TITLE);
 	if (!data->user)
 		return (free_data(data), 1);
 	data->assets = load_assets(data->user->display);
