eef91d532f5c629da0ac44dc80a1f73b2e511781
akovtune <akovtune@student.codam.nl>
2025-01-18 17:17:49 +0100

    Created printer.h Now I have a printer.c file located in the src/shared folder. It contains 2 functions: - print_success - print_failure

---
+++ src/shared/printer.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printer.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/18 16:48:46 by akovtune          #+#    #+#             */
+/*   Updated: 2025/01/18 16:49:34 by akovtune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTER_H
+# define PRINTER_H
+
+# include "ft_printf.h"
+
+void	print_failure(char *str);
+void	print_success(char *str);
+
+#endif
